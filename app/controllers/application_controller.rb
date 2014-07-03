4f5eb203c9599b2e633828096c5b0da4d5fa6fc0
Marta Buda <bmarta1991@o2.pl>
2014-07-03 17:23:24 +0200

    add users views, author method, categories_controller spec pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
 
-  before_action :authenticate_user!
-
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end

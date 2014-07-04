0bd6322278b724ae9c51e2b01adacda067c85fbb
Marta Buda <bmarta1991@o2.pl>
2014-07-04 10:13:13 +0200

    validation of price, #average_rating, product spec pass :+1:

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,13 @@
 class Product < ActiveRecord::Base
-  validates_presence_of :title, :description
-  validates_numericality_of :price
+  validates_presence_of :title, :description, :price
+  validates :price, numericality: { greater_than_or_equal_to: 0},
+    format: { with: /(\A\d+)(\.\d{1,2})?\z/ }
+
   belongs_to :category
   belongs_to :user
   has_many :reviews
 
-  def price
-  end
-
   def average_rating
+    reviews.inject(0.0){ |sum, review| sum + review.rating } / reviews.length
   end
 end
